b42c10c55a96af99e814876026bc355103a3e2a6
Endryus-Montezano <endryusmontezano@live.com>
2026-02-22 17:05:30 -0300

    Inseri dados no sistema

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -1,7 +1,27 @@
 const Memory = require("../models/Memory")
 
-const createMemory = async(requestAnimationFrame, res) => {
-    res.json("Deu certo!")
+const createMemory = async(req, res) => {
+    try {
+        const {title, description} = req.body
+
+        const src = `image/${req.file.filename}`
+
+        if(!title || description) {
+            return res.status(400).json({msg: "Por favor, preencha todos os campos."})
+        }
+
+        const newMemory = new Memory({
+            title, src, description
+        })
+
+        await newMemory.save()
+
+        res.json({msg: "Memória criada com sucesso!", newMemory})
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Ocorreu um erro!")
+    }
 }
 
 module.exports = {
