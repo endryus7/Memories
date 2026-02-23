a970959a073cfc2e1997c8ec4f4511a93c048bd7
Endryus-Montezano <endryusmontezano@live.com>
2026-02-23 19:47:59 -0300

    Resgata memórias  na home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import axios from '../axios-config'
+
+import { useState, useEffect } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './Home.css'
 
 const Home = () => {
+    const [memories, setMemories] = useState([])
+
+    useEffect(() => {
+        const getMemories = async() => {
+            const res = await axios.get("/memories")
+            
+            setMemories(res.data)
+        }
+
+        getMemories()
+    }, [])
+
   return (
-    <div>Home</div>
+    <div className='home'>
+        <h2>Confira as últimas Memórias</h2>
+        <div className="memories-container">
+            {memories.length > 0 && memories.map((memory) => <p>{memory.title}</p>)}
+        </div>
+    </div>
   )
 }
 
