349738fef023429335c9aca1e06461e938d5139d
Endryus-Montezano <endryusmontezano@live.com>
2026-02-23 19:54:38 -0300

    CSS da home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -23,7 +23,15 @@ const Home = () => {
     <div className='home'>
         <h2>Confira as últimas Memórias</h2>
         <div className="memories-container">
-            {memories.length > 0 && memories.map((memory) => <p>{memory.title}</p>)}
+            {memories.length > 0 && memories.map((memory) => 
+                <div className='memory' key={memory._id}>
+                    <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+                    <p>{memory.title}</p>
+                    <Link className='btn' to={`/memories/${memory._id}`}>
+                        Comentar
+                    </Link>
+                </div>
+            )}
         </div>
     </div>
   )
