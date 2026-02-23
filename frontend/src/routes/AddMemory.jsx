a76181e9c4ea45aea962130d3c3dd38d84c4088c
Endryus-Montezano <endryusmontezano@live.com>
2026-02-23 17:48:29 -0300

    Cria formulário de memória e estilos

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import axios from "axios"
+
+import { useState } from "react"
+
+import "./AddMemory.css"
 
 const AddMemory = () => {
   return (
-    <div>AddMemory</div>
+    <div className="add-memory-page">
+        <h2>Crie uma nova memória</h2>
+        <form>
+            <label>
+                <p>Título:</p>
+                <input 
+                type="text" 
+                placeholder="Defina um título" 
+                name="title"
+                />
+            </label>
+            <label>
+                <p>Descrição:</p>
+                <textarea 
+                placeholder="Explique o que aconteceu..." 
+                name="description"
+                ></textarea>
+            </label>
+            <label>
+                <p>Foto:</p>
+                <input 
+                type="file"  
+                name="image"
+                />
+            </label>
+            <input type="submit" value="Enviar" className="btn"/>
+        </form>
+    </div>
   )
 }
 
