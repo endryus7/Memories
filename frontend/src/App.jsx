8995251d393ecbb56640889201939d6758d997e7
Endryus-Montezano <endryusmontezano@live.com>
2026-02-23 17:23:34 -0300

    Configura React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+
 function App() {
   
 
   return (
     <div className='App'>
       <h1>Memories</h1>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   )
 }
