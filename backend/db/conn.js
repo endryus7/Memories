1fb75d407286f434ea08911fecd26eb3e0999186
Endryus-Montezano <endryusmontezano@live.com>
2026-03-07 22:37:28 -0300

    Modifica o conn

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose")
-
 require("dotenv").config()
 
 mongoose.set("strictQuery", true)
 
 async function main() {
-    await mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.ts5gja1.mongodb.net/?appName=Cluster0`)
 
-    console.log("Conectado com sucesso!")
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.ts5gja1.mongodb.net/memories?retryWrites=true&w=majority`
+  )
+
+  console.log("Conectado com sucesso!")
 }
 
 main().catch((err) => console.log(err))
