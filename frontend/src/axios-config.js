c68fc82e6dcbecffbcdcb802ede058a4ca2d09bc
Endryus-Montezano <endryusmontezano@live.com>
2026-03-07 22:26:08 -0300

    Arruma url do axios

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://memories-95kh.onrender.com/memories"
+axios.defaults.baseURL = "https://memories-95kh.onrender.com"
 
 axios.defaults.headers.post["Content-Type"] = "application/json"
 
