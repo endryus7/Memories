a0b5b4aa89d01ae6835f1f8f938d62763d8cbbc6
Endryus-Montezano <endryusmontezano@live.com>
2026-03-07 22:10:19 -0300

    Modifica url do axios

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "http://localhost:3000/"
+axios.defaults.baseURL = "https://memories-95kh.onrender.com/memories"
 
 axios.defaults.headers.post["Content-Type"] = "application/json"
 
